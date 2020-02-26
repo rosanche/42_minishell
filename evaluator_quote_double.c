613dafc7bff841f54b51f5a411ab6ceb0b749912
Enzo CACERES <ecaceres@e2r2p6.42.fr>
2020-02-26 12:08:46 +0100

    Normed quote evaluator

--- evaluator_quote_double.c
+++ evaluator_quote_double.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   evaluator_quote.c                                  :+:      :+:    :+:   */
+/*   evaluator_quote_double.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecaceres <ecaceres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,73 +12,37 @@
 
 #include "minishell.h"
 
-int
-	eval_q_single(char *line, size_t *consumed, t_arrlst *chrlst)
+static void
+	commit_escape(char **line, size_t *consumed, t_arrlst *chrlst)
 {
 	int		ret;
 	size_t	sub;
 
-	while (*line)
+	if (*(*line + 1) == '\"')
 	{
-		if (*line == '\\')
-		{
-			if (*(line + 1) == '\'')
-			{
-				arg_builder_add_char(chrlst, '\\', '\'');
-				*consumed += 2;
-				return (0);
-			}
-			else
-			{
-				ret = eval_escape_backslash(line, &sub);
-				if (ret != -1)
-				{
-					arg_builder_add_char(chrlst, ret, '\'');
-					*consumed += sub;
-					line += *consumed;
-				}
-			}
-		}
-		else if (*line == '\'')
-		{
-			*consumed += 1;
-			return (0);
-		}
-		else
-			arg_builder_add_char(chrlst, *line, '\'');
-		*consumed += 1;
-		line++;
+		arg_builder_add_char(chrlst, '\"', '\"');
+		eval_consume(1, line, consumed, 0);
 	}
-	return (0);
+	else
+	{
+		ret = eval_escape_backslash(*line, &sub);
+		if (ret != -1)
+		{
+			arg_builder_add_char(chrlst, ret, '\"');
+			eval_consume(sub, line, consumed, 0);
+		}
+	}
 }
 
 int
 	eval_q_double(char *line, size_t *consumed, t_arrlst *chrlst)
 {
-	int		ret;
 	size_t	sub;
 
 	while (*line)
 	{
 		if (*line == '\\')
-		{
-			if (*(line + 1) == '\"')
-			{
-				line++;
-				arg_builder_add_char(chrlst, '\"', '\"');
-				*consumed += 1;
-			}
-			else
-			{
-				ret = eval_escape_backslash(line, &sub);
-				if (ret != -1)
-				{
-					arg_builder_add_char(chrlst, ret, '\"');
-					*consumed += sub;
-					line += sub;
-				}
-			}
-		}
+			commit_escape(&line, consumed, chrlst);
 		else if (*line == '\"')
 		{
 			*consumed += 1;
